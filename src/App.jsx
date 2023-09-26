8b326c9252457ba8651143fc925570f8eab266bb
Zaid <zaydssh500@gmail.com>
2023-09-26 16:44:21 +0100

    in progress

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { AddTransaction } from './Components/AddTransaction';
+import Balance from './Components/Balance';
+import { Header } from './Components/Header';
+import IncomeExpenses from './Components/IncomeExpenses';
+import TransactionList from './Components/TransactionList';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
